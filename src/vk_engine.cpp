b35473bbbdde576e9d52f0f708f92715d45fa40a
Hans Wannop <hanswannop@me.com>
2024-12-09 03:23:15 +0100

    Vulkan init: setup sdl surface, select physical device, make vulkan device and swapchain.

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,10 +7,14 @@
 #include <vk_initializers.h>
 #include <vk_types.h>
 
+// Vulkan bootstrap library, can remove later setup done from scratch using Vulkan tutorial
+// https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Base_code
+#include "VkBootstrap.h"
+
 #include <chrono>
 #include <thread>
 
-constexpr bool bUseValidationLayers = false;
+constexpr bool bUseValidationLayers = true;
 
 VulkanEngine* loadedEngine = nullptr;
 
@@ -34,14 +38,126 @@ void VulkanEngine::init()
         _windowExtent.height,
         window_flags);
 
+	init_vulkan();
+
+	init_swapchain();
+
+	init_commands();
+
+	init_sync_structures();
+
     // everything went fine
     _isInitialized = true;
 }
 
+void VulkanEngine::init_vulkan()
+{
+	// Use VkBootsrap to make a Vulkan 1.3 instance 
+	// with basic debug features active.
+	// TODO: read vulkan tutorial and do this manually
+	vkb::InstanceBuilder builder;
+
+	auto inst_ret = builder.set_app_name("Vulkan Engine")
+	.request_validation_layers(bUseValidationLayers)
+	.use_default_debug_messenger()
+	.require_api_version(1, 3, 0)
+	.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	// Get the resulting instance from Vulkan bootstrap
+	_instance = vkb_inst.instance;
+	_debug_messenger = vkb_inst.debug_messenger;
+
+	// Create the VkSurfaceKHR object for the SDL window
+	SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+	// Select vulkan 1.3 features
+	VkPhysicalDeviceVulkan13Features features13 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+	features13.dynamicRendering = true;
+	features13.synchronization2 = true;
+
+	// Select vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+	// Select the physical device with our chosen features using vkbootstrap that can render to our surface
+	vkb::PhysicalDeviceSelector selector { vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+	.set_minimum_version(1, 3)
+	.set_required_features_13(features13)
+	.set_required_features_12(features12)
+	.set_surface(_surface)
+	.select()
+	.value();
+
+	// Make the final vulkan device
+	vkb::DeviceBuilder deviceBuilder { physicalDevice };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the handles from the vkbootsrap builders
+	_device = vkbDevice.device;
+	_chosenGPU = physicalDevice.physical_device;
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+	vkb::SwapchainBuilder swapchainBuilder { _chosenGPU, _device, _surface };
+	_swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+	
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+	.set_desired_format(VkSurfaceFormatKHR { .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+	.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)  // Hard vsync, limit fps of engine to vsync
+	.set_desired_extent(width,height)
+	.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+	.build()
+	.value();
+
+	_swapchainExtent = vkbSwapchain.extent;
+
+	// Store swapchain and its images
+	_swapchain = vkbSwapchain.swapchain;
+	_swapchainImages = vkbSwapchain.get_images().value();
+	_swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+	// Delete swapchain, this will also delete the images it holds
+	vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+	// Delete the ImageViews associated
+	for (int i = 0; i < _swapchainImageViews.size(); i++) {
+		vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+	}
+}
+
+void VulkanEngine::init_swapchain() 
+{
+	// Create swapchain with default size as the window size
+	create_swapchain(_windowExtent.width, _windowExtent.height); 
+}
+
+void VulkanEngine::init_commands()
+{
+}
+
+void VulkanEngine::init_sync_structures()
+{
+}
+
 void VulkanEngine::cleanup()
 {
-    if (_isInitialized) {
+	if (_isInitialized) {
+		destroy_swapchain();
+		vkDestroyInstance(_instance, nullptr);
+		vkDestroyDevice(_device, nullptr);
+		vkDestroySurfaceKHR(_instance, _surface, nullptr);
+		// Note, we dont destroy VKPhysical device, its not a Vulkan resource, just a handle to the GPU
 
+		vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+		
         SDL_DestroyWindow(_window);
     }
 
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -13,12 +13,36 @@ public:
 	bool stop_rendering{ false };
 	VkExtent2D _windowExtent{ 1440 , 900 };
 
+	VkInstance _instance; // Vulkan instance handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen to be used as default device
+	VkDevice _device; // Vulkan logical device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;  // Actual image objects to render into
+	std::vector<VkImageView> _swapchainImageViews; // Wrappers for these images
+	VkExtent2D _swapchainExtent;
+
 	struct SDL_Window* _window{ nullptr };
 
 	static VulkanEngine& Get();
 
-	//initializes everything in the engine
+	// Initializes everything in the engine
 	void init();
+
+	// Initializes vulkan instance
+	void init_vulkan();
+
+	// Initializes swapchain
+	void init_swapchain();
+
+	// Initializes command buffer
+	void init_commands();
+
+	// Initialises synchronisation structures
+	void init_sync_structures();
 
 	//shuts down the engine
 	void cleanup();
@@ -28,4 +52,9 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	// We need to rebuild swapchain as window resizes, so we have them separated from init_swapchain();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
